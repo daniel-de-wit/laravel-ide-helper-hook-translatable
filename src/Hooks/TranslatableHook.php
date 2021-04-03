ce7ed1fcadc974e691f9e7498ac4c3718e662b84
wimski <wimreckman@gmail.com>
2021-04-03 14:06:38 +0200

    Refactor early return

--- src/Hooks/TranslatableHook.php
+++ src/Hooks/TranslatableHook.php
@@ -13,93 +13,91 @@ class TranslatableHook implements ModelHookInterface
 {
     public function run(ModelsCommand $command, Model $model): void
     {
-        if (!$model instanceof Translatable) {
+        if (!$model instanceof Translatable || !method_exists($model, 'getTranslationModelName')) {
             return;
         }
 
-        if (method_exists($model, 'getTranslationModelName')) {
-            $className = $model->getTranslationModelName();
-            $modelTranslation = new $className;
+        $className = $model->getTranslationModelName();
+        $modelTranslation = new $className;
 
-            $table = $modelTranslation->getConnection()->getTablePrefix() . $modelTranslation->getTable();
-            $schema = $modelTranslation->getConnection()->getDoctrineSchemaManager();
-            $databasePlatform = $schema->getDatabasePlatform();
-            $databasePlatform->registerDoctrineTypeMapping('enum', 'string');
+        $table = $modelTranslation->getConnection()->getTablePrefix() . $modelTranslation->getTable();
+        $schema = $modelTranslation->getConnection()->getDoctrineSchemaManager();
+        $databasePlatform = $schema->getDatabasePlatform();
+        $databasePlatform->registerDoctrineTypeMapping('enum', 'string');
 
-            $platformName = $databasePlatform->getName();
-            $customTypes = config()->get("ide-helper.custom_db_types.{$platformName}", []);
-            foreach ($customTypes as $yourTypeName => $doctrineTypeName) {
-                $databasePlatform->registerDoctrineTypeMapping($yourTypeName, $doctrineTypeName);
+        $platformName = $databasePlatform->getName();
+        $customTypes = config()->get("ide-helper.custom_db_types.{$platformName}", []);
+        foreach ($customTypes as $yourTypeName => $doctrineTypeName) {
+            $databasePlatform->registerDoctrineTypeMapping($yourTypeName, $doctrineTypeName);
+        }
+
+        $database = null;
+        if (strpos($table, '.')) {
+            [$database, $table] = explode('.', $table);
+        }
+
+        $columns = $schema->listTableColumns($table, $database);
+
+        if (!$columns) {
+            return;
+        }
+
+        foreach ($columns as $column) {
+            $name = $column->getName();
+
+            if (! in_array($name, $model->translatedAttributes)) {
+                continue;
             }
 
-            $database = null;
-            if (strpos($table, '.')) {
-                [$database, $table] = explode('.', $table);
-            }
-
-            $columns = $schema->listTableColumns($table, $database);
-
-            if (!$columns) {
-                return;
-            }
-
-            foreach ($columns as $column) {
-                $name = $column->getName();
-
-                if (! in_array($name, $model->translatedAttributes)) {
-                    continue;
+            if (in_array($name, $modelTranslation->getDates())) {
+                $type = $this->dateClass;
+            } else {
+                $type = $column->getType()->getName();
+                switch ($type) {
+                    case 'string':
+                    case 'text':
+                    case 'date':
+                    case 'time':
+                    case 'guid':
+                    case 'datetimetz':
+                    case 'datetime':
+                    case 'decimal':
+                        $type = 'string';
+                        break;
+                    case 'integer':
+                    case 'bigint':
+                    case 'smallint':
+                        $type = 'integer';
+                        break;
+                    case 'boolean':
+                        switch (config('database.default')) {
+                            case 'sqlite':
+                            case 'mysql':
+                                $type = 'integer';
+                                break;
+                            default:
+                                $type = 'boolean';
+                                break;
+                        }
+                        break;
+                    case 'float':
+                        $type = 'float';
+                        break;
+                    default:
+                        $type = 'mixed';
+                        break;
                 }
-
-                if (in_array($name, $modelTranslation->getDates())) {
-                    $type = $this->dateClass;
-                } else {
-                    $type = $column->getType()->getName();
-                    switch ($type) {
-                        case 'string':
-                        case 'text':
-                        case 'date':
-                        case 'time':
-                        case 'guid':
-                        case 'datetimetz':
-                        case 'datetime':
-                        case 'decimal':
-                            $type = 'string';
-                            break;
-                        case 'integer':
-                        case 'bigint':
-                        case 'smallint':
-                            $type = 'integer';
-                            break;
-                        case 'boolean':
-                            switch (config('database.default')) {
-                                case 'sqlite':
-                                case 'mysql':
-                                    $type = 'integer';
-                                    break;
-                                default:
-                                    $type = 'boolean';
-                                    break;
-                            }
-                            break;
-                        case 'float':
-                            $type = 'float';
-                            break;
-                        default:
-                            $type = 'mixed';
-                            break;
-                    }
-                }
-
-                $comment = $column->getComment();
-                $command->setProperty(
-                    $name,
-                    $type,
-                    true,
-                    true,
-                    $comment,
-                    true,
-                );
             }
+
+            $comment = $column->getComment();
+            $command->setProperty(
+                $name,
+                $type,
+                true,
+                true,
+                $comment,
+                true,
+            );
         }
     }
 }
