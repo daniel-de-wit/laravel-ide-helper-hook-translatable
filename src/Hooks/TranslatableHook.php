a63bfb9da485d50e723eedb0ab148c306d743d25
Daniel de Wit <daniel@danieldewit.nl>
2021-04-14 14:39:10 +0200

    Fix static code warnings

--- src/Hooks/TranslatableHook.php
+++ src/Hooks/TranslatableHook.php
@@ -17,89 +17,88 @@ class TranslatableHook implements ModelHookInterface
             return;
         }
 
-        if (method_exists($model, 'getTranslationModelName')) {
-            $className = $model->getTranslationModelName();
-            $modelTranslation = new $className;
+        if (! method_exists($model, 'getTranslationModelName') || ! property_exists($model, 'translatedAttributes')) {
+            return;
+        }
 
-            $table = $modelTranslation->getConnection()->getTablePrefix() . $modelTranslation->getTable();
-            $schema = $modelTranslation->getConnection()->getDoctrineSchemaManager();
-            $databasePlatform = $schema->getDatabasePlatform();
-            $databasePlatform->registerDoctrineTypeMapping('enum', 'string');
+        $className = $model->getTranslationModelName();
+        $modelTranslation = new $className;
 
-            $platformName = $databasePlatform->getName();
-            $customTypes = config()->get("ide-helper.custom_db_types.{$platformName}", []);
-            foreach ($customTypes as $yourTypeName => $doctrineTypeName) {
-                $databasePlatform->registerDoctrineTypeMapping($yourTypeName, $doctrineTypeName);
+        $table = $modelTranslation->getConnection()->getTablePrefix() . $modelTranslation->getTable();
+        $schema = $modelTranslation->getConnection()->getDoctrineSchemaManager();
+        $databasePlatform = $schema->getDatabasePlatform();
+        $databasePlatform->registerDoctrineTypeMapping('enum', 'string');
+
+        $platformName = $databasePlatform->getName();
+        $customTypes = config()->get("ide-helper.custom_db_types.{$platformName}", []);
+
+        foreach ($customTypes as $yourTypeName => $doctrineTypeName) {
+            $databasePlatform->registerDoctrineTypeMapping($yourTypeName, $doctrineTypeName);
+        }
+
+        $database = null;
+        if (strpos($table, '.')) {
+            [$database, $table] = explode('.', $table);
+        }
+
+        $columns = $schema->listTableColumns($table, $database);
+
+        if (!$columns) {
+            return;
+        }
+
+        foreach ($columns as $column) {
+            $name = $column->getName();
+
+            if (! in_array($name, $model->translatedAttributes)) {
+                continue;
             }
 
-            $database = null;
-            if (strpos($table, '.')) {
-                [$database, $table] = explode('.', $table);
-            }
-
-            $columns = $schema->listTableColumns($table, $database);
-
-            if (!$columns) {
-                return;
-            }
-
-            foreach ($columns as $column) {
-                $name = $column->getName();
-
-                if (! in_array($name, $model->translatedAttributes)) {
-                    continue;
-                }
-
-                if (in_array($name, $modelTranslation->getDates())) {
-                    $type = $this->dateClass;
-                } else {
-                    $type = $column->getType()->getName();
-                    switch ($type) {
-                        case 'string':
-                        case 'text':
-                        case 'date':
-                        case 'time':
-                        case 'guid':
-                        case 'datetimetz':
-                        case 'datetime':
-                        case 'decimal':
-                            $type = 'string';
-                            break;
-                        case 'integer':
-                        case 'bigint':
-                        case 'smallint':
+            $type = $column->getType()->getName();
+            switch ($type) {
+                case 'string':
+                case 'text':
+                case 'date':
+                case 'time':
+                case 'guid':
+                case 'datetimetz':
+                case 'datetime':
+                case 'decimal':
+                    $type = 'string';
+                    break;
+                case 'integer':
+                case 'bigint':
+                case 'smallint':
+                    $type = 'integer';
+                    break;
+                case 'boolean':
+                    switch (config('database.default')) {
+                        case 'sqlite':
+                        case 'mysql':
                             $type = 'integer';
                             break;
-                        case 'boolean':
-                            switch (config('database.default')) {
-                                case 'sqlite':
-                                case 'mysql':
-                                    $type = 'integer';
-                                    break;
-                                default:
-                                    $type = 'boolean';
-                                    break;
-                            }
-                            break;
-                        case 'float':
-                            $type = 'float';
-                            break;
                         default:
-                            $type = 'mixed';
+                            $type = 'boolean';
                             break;
                     }
-                }
-
-                $comment = $column->getComment();
-                $command->setProperty(
-                    $name,
-                    $type,
-                    true,
-                    true,
-                    $comment,
-                    true,
-                );
+                    break;
+                case 'float':
+                    $type = 'float';
+                    break;
+                default:
+                    $type = 'mixed';
+                    break;
             }
+
+            $comment = $column->getComment();
+            $command->setProperty(
+                $name,
+                $type,
+                true,
+                true,
+                $comment,
+                true,
+            );
         }
     }
 }
