fbb7460d2b404e554c30e47e7893b99722c906ed
Daniël de Wit <daniel@danieldewit.nl>
2025-01-24 18:46:50 +0100

    Upgrade

--- src/Providers/LaravelIdeHelperHookTranslatableServiceProvider.php
+++ src/Providers/LaravelIdeHelperHookTranslatableServiceProvider.php
@@ -1,19 +1,49 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DanielDeWit\LaravelIdeHelperHookTranslatable\Providers;
 
+use Barryvdh\LaravelIdeHelper\Console\ModelsCommand;
 use DanielDeWit\LaravelIdeHelperHookTranslatable\Hooks\TranslatableHook;
+use Illuminate\Console\Command;
 use Illuminate\Contracts\Config\Repository as Config;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class LaravelIdeHelperHookTranslatableServiceProvider extends ServiceProvider
+class LaravelIdeHelperHookTranslatableServiceProvider extends ServiceProvider implements DeferrableProvider
 {
+    /**
+     * @var string
+     */
+    const ModelsCommandAlias = 'laravel-ide-helper-hook-translatable-command-alias';
+
+    /**
+     * @return list<class-string<Command>|string>
+     */
+    public function provides(): array
+    {
+        return [
+            static::ModelsCommandAlias,
+        ];
+    }
+
+    public function boot(): void
+    {
+        // Laravel only allows a single deferred service provider to claim
+        // responsibility for a given class, interface, or service in the
+        // provides() method. To ensure this provider is properly loaded
+        // when running the ModelsCommand we bind an alias and use that instead.
+        $this->app->alias(ModelsCommand::class, static::ModelsCommandAlias);
+    }
+
     public function register(): void
     {
-        if ($this->app->isProduction()) {
-            return;
-        }
+        $this->registerIdeHelperHook();
+    }
 
+    protected function registerIdeHelperHook(): void
+    {
         /** @var Config $config */
         $config = $this->app->get('config');
 
